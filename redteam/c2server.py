8c10a8887146a77668433ace6593c1c4aaa1d5dd
lmcewen9 <89426396+lmcewen9@users.noreply.github.com>
2024-10-23 21:47:34 -0400

    hopefully

--- redteam/c2server.py
+++ redteam/c2server.py
@@ -12,7 +12,7 @@ class C2Server:
     
     def start_server(self):
         self.server.bind((self.host, self.port))
-        self.server.listen(10)
+        self.server.listen(5)
         print(f"[*] C2 server started on {self.host}:{self.port}")
 
         while True:
@@ -20,7 +20,7 @@ class C2Server:
             print(f"[*] Connection establisted from {addr}")
 
             with self.lock:
-                self.clients[addr] = client
+                self.clients[addr.index(1)] = client
                 client_thread = threading.Thread(target=self.handle_client, args=(client, addr))
                 client_thread.start()
     
@@ -37,7 +37,7 @@ class C2Server:
             except Exception:
                 print(f"[*] Connection lost with {addr}")
                 with self.lock:
-                    del self.clients[addr]
+                    del self.clients[addr.index(1)]
                 break
     
     def switch_client(self):
